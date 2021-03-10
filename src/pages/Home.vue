2e040df3a7b943c0b498c26103a3bea1c1e48ccc
Zozylya Nikita <n.zozylya@riverstart.ru>
2021-03-10 13:43:59 +0300

    search was created and worked, but needs further development

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -15,12 +15,36 @@
         >
           Search
         </button>
+
+        <p v-if="error">
+          {{ error }}
+        </p>
+
+        <!-- repos -->
+        <div class="repos__wrapper" v-if="repos">
+          <!-- repo item -->
+          <h2>
+            Repositories
+          </h2>
+          <a target="_blank" :href="repo.html_url" class="repo__item" v-for='repo in repos' :key='repo.id'>
+            <div class="repo__info">
+              <span class="link">{{repo.name}}</span>
+              <span>
+                {{ repo.stargazers_count }} ⭐
+              </span>
+            </div>
+          </a>
+        </div>
       </div>
     </section>
   </div>
 </template>
 
 <script>
+// libs
+import axios from 'axios';
+
+// components
 import search from '@/components/Search.vue';
 
 
@@ -31,11 +55,34 @@ export default {
   data() {
     return {
       search: '',
+      repos: null,
+      users: null,
+      error: null,
     }
   },
   methods: {
+    // 
     getRepos () {
-      console.log(`get user ${this.search} repos`)
+      axios
+        .get(`https://api.github.com/users/${this.search}/repos`)
+          .then(res => {
+            console.log('repos: ' + res.data);
+            this.repos = res.data;
+            this.error = null
+          })
+          .catch(err => {
+            this.error = 'Can not find this user'
+            this.repos = null;
+          });
+      // axios
+      //   .get(`https://api.github.com/users/${this.search}`)
+      //     .then(res => {
+      //       console.log('users: ' + res.data);
+      //     })
+      //     .catch(err => {
+      //       this.error = 'Can not find this user'
+      //       this.repos = null;
+      //     })
     }
   }
 }
@@ -49,5 +96,38 @@ export default {
   }
   .btn {
     margin-top: 20px;
+    transition: .4s ease-in;
+    &:hover {
+      opacity: .8;
+    }
+  }
+ 
+  .repos__wrapper {
+    width: 600px;
+    margin: 30px 0;
+  }
+  h2 {
+    margin-bottom: 30px;
+  }
+  .repo__item {
+    margin-bottom: 15px;
+    padding: 10px 5px;
+    border: rosybrown solid 1px;
+    display: block;
+    transition: .2s ease-in-out;
+    .link {
+      border: none;
+    }
+    &:hover {
+      background-color: rgba(#dbdbdb, .9);
+      .link {
+          color: rgb(238, 98, 98);
+      }
+    }
+    .repo__info {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
   }
 </style>
